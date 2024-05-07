2d710924a0b6e737995e59ae8a08e6b38d5dec95
Present111 <22521118@gm.uit.edu.vn>
2024-05-07 20:43:11 +0700

    Update phần all friend v2

--- app/src/main/java/com/example/myapplication/fragments/ViewPostFragment.java
+++ app/src/main/java/com/example/myapplication/fragments/ViewPostFragment.java
@@ -25,10 +25,16 @@ import androidx.core.content.ContextCompat;
 
 import com.example.myapplication.Gesture.SimpleGestureFilter;
 import com.example.myapplication.R;
+import com.example.myapplication.activities.ChatActivity;
+import com.example.myapplication.activities.MainActivity;
 import com.example.myapplication.activities.RecentChatActivity;
 import com.example.myapplication.activities.SearchUserActivity;
+import com.example.myapplication.adapter.AddFriendAdapter;
+import com.example.myapplication.adapter.FriendAdapter;
 import com.example.myapplication.adapter.RecentChatRecyclerAdapter;
+import com.example.myapplication.interfaces.AddFriend;
 import com.example.myapplication.models.Chatroom;
+import com.example.myapplication.models.User;
 import com.example.myapplication.utils.FirebaseUtils;
 import com.firebase.ui.firestore.FirestoreRecyclerOptions;
 import com.google.firebase.firestore.Query;
@@ -55,19 +61,41 @@ import androidx.core.view.WindowInsetsCompat;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
+import java.util.ArrayList;
+import java.util.List;
+
 /**
  * A simple {@link Fragment} subclass.
  * Use the {@link ViewPostFragment#newInstance} factory method to
  * create an instance of this fragment.
  */
-public class ViewPostFragment extends Fragment {
+public class ViewPostFragment extends Fragment implements AddFriend{
+    @Override
+    public void onAddFriend(String userId, String username, int position) {
+        // Implement logic khi thêm bạn bè
+    }
+
+    @Override
+    public void unFriend(String userId, int position) {
+        // Implement logic khi hủy kết bạn
+    }
+    @Override
+    public void onClick(User user) {
+        // Implement logic khi click vào user
+        btnalluser.setText(user.getUsername());
+        popupWindow.dismiss();
+
+    }
     Button btnalluser,btnall;
     RelativeLayout layout;
 
-    RecyclerView recyclerView;
+    RecyclerView rcvlistfriend;
     RecentChatRecyclerAdapter adapter;
 
     private SimpleGestureFilter detector;
+    private FriendAdapter friendAdapter;
+    PopupWindow popupWindow;
+    View popUpView;
 
 
 
@@ -87,34 +115,43 @@ public class ViewPostFragment extends Fragment {
             public void onClick(View v) {
                 LayoutInflater inflater = (LayoutInflater) getContext().getSystemService(Context.LAYOUT_INFLATER_SERVICE);
 
-                View popUpView =inflater.inflate(R.layout.activity_allfriend,null);
+                popUpView =inflater.inflate(R.layout.activity_allfriend,null);
 
                 int width = ViewGroup.LayoutParams.MATCH_PARENT;
                 int height=ViewGroup.LayoutParams.MATCH_PARENT;
                 boolean focusable=true;
-                PopupWindow popupWindow= new PopupWindow(popUpView,width,height,focusable);
+                popupWindow= new PopupWindow(popUpView,width,height,focusable);
                 popupWindow.showAtLocation(layout, Gravity.CENTER, 0, 0);
 
-                recyclerView=popUpView.findViewById(R.id.recyler_view_allfriend);
+                rcvlistfriend=popUpView.findViewById(R.id.listfriend);
+                FirebaseUtils.currentUserDetail().get().addOnCompleteListener(task -> {
+                    if (task.isSuccessful()) {
+                        User user = task.getResult().toObject(User.class);
+                        if (user != null && user.getFriends() != null) {
+                            for (int i = 0; i < user.getFriends().size(); i++) {
+                                FirebaseUtils.allUserCollectionReference().document(user.getFriends().get(i)).get().addOnCompleteListener(task1 -> {
+                                    if (task1.isSuccessful()) {
+                                        User friend = task1.getResult().toObject(User.class);
+                                        if (friend != null) {
+                                            friendAdapter.addItem(friend);
+                                        }
+                                    }
+                                });
+                            }
+                        }
+                    }
+                });
+                friendAdapter = new FriendAdapter(true, ViewPostFragment.this);
+                rcvlistfriend.setAdapter(friendAdapter);
                 btnall=popUpView.findViewById(R.id.btnall);
-
-                Query query = FirebaseUtils.allChatroomCollectionReference()
-                        .whereArrayContains("userIds", FirebaseUtils.currentUserID())
-                        .orderBy("lastMessageTimestamp", Query.Direction.DESCENDING);
-
-                FirestoreRecyclerOptions<Chatroom> options = new FirestoreRecyclerOptions.Builder<Chatroom>().setQuery(query, Chatroom.class).build();
-                adapter = new RecentChatRecyclerAdapter(options, getContext().getApplicationContext());
-                recyclerView.setLayoutManager(new LinearLayoutManager(requireContext()));
-                recyclerView.setAdapter(adapter);
-                adapter.startListening();
-
                 btnall.setOnClickListener(new View.OnClickListener() {
                     @Override
                     public void onClick(View v) {
-                        Intent i= new Intent(requireContext(),RecentChatActivity.class);
-                        startActivity(i);
+                        btnalluser.setText("Tất cả bạn bè");
+                        popupWindow.dismiss();
                     }
                 });
+
 
                 popUpView.setOnTouchListener(new View.OnTouchListener() {
                     @Override
@@ -126,6 +163,18 @@ public class ViewPostFragment extends Fragment {
 
             }
         });
+    }
+    private List<User> filterFriends(List<User> data) {
+        ArrayList<User> newList = new ArrayList<>(data);
+        for (int i = 0; i < data.size(); i++) {
+            for (User user : friendAdapter.getList()) {
+                if (data.get(i).getUserId().equals(user.getUserId())) {
+                    newList.remove(data.get(i));
+                    break;
+                }
+            }
+        }
+        return newList;
     }
 
 
